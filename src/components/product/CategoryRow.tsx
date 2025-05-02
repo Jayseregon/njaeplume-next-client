992d510cb11a68a0baa5ea2593ac0a85af01a3f7
jayseregon <jayseregon@gmail.com>
2025-05-01 21:19:34 -0400

    feat: update product display on store front page

--- src/components/product/CategoryRow.tsx
+++ src/components/product/CategoryRow.tsx
@@ -6,36 +6,34 @@ import { ArrowRight } from "lucide-react";
 
 import { CategoryRowProps } from "@/src/interfaces/Products";
 import { ProductCard } from "@/src/components/product/ProductCard";
-import { SectionTitle } from "@/src/components/root/SectionTitle";
 import { getNavItemByKey } from "@/config/site";
 
 export const CategoryRow = ({ category, products }: CategoryRowProps) => {
   const t = useTranslations("Shop");
-  const displayProducts = products.slice(0, 3);
+  const displayProducts = products.slice(0, 4);
   const categoryNavItem = getNavItemByKey(category);
   const categoryTitle = t(`categoryTitles.${category}`);
 
   return (
-    <div className="my-8">
+    <div className="my-20">
       <div className="mb-4">
-        <SectionTitle title={categoryTitle} />
+        <h2 className="text-2xl font-bold mb-3">{categoryTitle}</h2>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
         {displayProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
-
-        <Link
-          className="flex flex-col justify-center items-center border-2 border-dashed border-muted rounded-2xl p-8 my-auto self-center transition-all duration-200 hover:border-primary hover:shadow-md hover:scale-[1.02]"
-          href={categoryNavItem.href}
-        >
-          <ArrowRight className="h-8 w-8 mb-2" />
-          <span className="text-lg font-medium">
-            {t(`seeAllButton.${category}`)}
-          </span>
-        </Link>
       </div>
+      <Link
+        className="flex flex-row justify-center items-center w-full border-2 border-dashed border-muted rounded-2xl py-4 px-8 mt-6 transition-all duration-200 hover:border-primary hover:shadow-md hover:scale-[1.02]"
+        href={categoryNavItem.href}
+      >
+        <ArrowRight className="h-6 w-6 mr-2 text-foreground" />
+        <span className="text-lg font-medium text-foreground">
+          {t(`seeAllButton.${category}`)}
+        </span>
+      </Link>
     </div>
   );
 };
