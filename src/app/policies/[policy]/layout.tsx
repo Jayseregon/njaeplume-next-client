8749055480dc1a0a590c7a7d1063ff7f309afa71
jayseregon <jayseregon@gmail.com>
2025-05-09 16:57:44 -0400

    feat: implement termageddon policies

---
+++ src/app/policies/[policy]/layout.tsx
@@ -0,0 +1,13 @@
+export default function PolicyLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-6 md:py-10">
+      <div className="inline-block max-w-4xl text-center justify-center">
+        {children}
+      </div>
+    </section>
+  );
+}
